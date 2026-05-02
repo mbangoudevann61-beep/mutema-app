896666f598bd18fb091c1de659f69350a270dcb4
mbangoudevann61-beep <mbangoudevann61@gmail.com>
2026-05-02 22:10:59 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("mutema").then(cache => cache.addAll(["./"]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
